4e029236bbd64142ccb24ecb16a67652c449498a
Tim LaBerge <tlaberge@godaddy.com>
2018-04-28 12:06:53 -0700

    Add support for storing data in S3

--- Entry.py
+++ Entry.py
@@ -3,29 +3,34 @@ from datetime import datetime
 from time import time
 from tempfile import NamedTemporaryFile
 from os import rename
+from html import escape, unescape
+
+import boto3
 
 
 class Entry(object):
+    s3 = boto3.resource('s3')
+
     def __init__(self, text, timestamp=None):
         """
-        'text' is the entry's text. It will be formatted as <pre>entry</pre>, so escape >, <, etc.
+        'text' is the entry's text. Escape it so that we can use <,&, ; etc in the journal.
         'timestamp' is the seconds since the epoch, e.g., as returned by time.time()
         """
-        self.text = text
+        self.text = escape(text)
         if timestamp:
-            self.timestamp = timestamp
+            self.timestamp = float(timestamp)
         else:
             self.timestamp = time()
 
     def to_json_dict(self):
-        return {'text': self.text, 'timestamp': self.timestamp}
+        return {'text': self.text, 'timestamp': str(self.timestamp)}
 
     def __str__(self):
-        return "{}<br><br>{}".format(datetime.fromtimestamp(self.timestamp).strftime("%c"), self.text)
+        return "{}<br><br>{}".format(datetime.fromtimestamp(self.timestamp).strftime("%c"), unescape(self.text))
 
     @staticmethod
     def from_json_dict(json_dict):
-        return Entry(json_dict['text'], json_dict['timestamp'])
+        return Entry(json_dict['text'], float(json_dict['timestamp']))
 
     @staticmethod
     def entries_from_json_file(file_name):
@@ -50,3 +55,18 @@ class Entry(object):
         with NamedTemporaryFile('w', delete=False) as tmp_file:
             json.dump(json_entries, tmp_file)
             rename(tmp_file.name, file_name)
+
+    @classmethod
+    def get_entries_from_s3(cls, bucket_name):
+        entries = []
+        bucket = cls.s3.Bucket(bucket_name)
+        for key in bucket.objects.all():
+            resp = cls.s3.Object(bucket_name, key.key).get()
+            text = resp['Body'].read().decode()
+            entries.append(Entry(text, timestamp=key.key))
+
+        return entries
+
+    @classmethod
+    def push_entry_to_s3(cls, bucket_name, entry):
+        cls.s3.Object(bucket_name, str(entry.timestamp)).put(Body=entry.text, ServerSideEncryption='AES256')
--- journal.py
+++ journal.py
@@ -1,50 +1,55 @@
-from flask import Flask, render_template, request
 from argparse import ArgumentParser
+from flask import Flask, render_template, request
 from os import environ
 
 from Entry import Entry
 
-app = Flask(__name__)
-
-# entries = [ Entry("A sample entry")]
 entry_file = None
-s3_token = None
+bucket_name = None
+user = None
+
+app = Flask(__name__)
 
 
 @app.route('/', methods=['get', 'post'])
 def index():
-    global entries
+    if request.method == 'POST':
+        print(request.form.get('Entry'))
+        entry = Entry(request.form.get('Entry'))
+
+        if entry_file:
+            entries = Entry.entries_from_json_file(entry_file)
+            entries.append(entry)
+            Entry.entries_to_json_file(entry_file, entries)
+        else:
+            Entry.push_entry_to_s3(bucket_name, entry)
 
     if entry_file:
         entries = Entry.entries_from_json_file(entry_file)
     else:
-        entries = []
+        entries = Entry.get_entries_from_s3(bucket_name)
 
-    if request.method == 'POST':
-        entry = Entry(request.form.get('Entry'))
-        entries.append(entry)
-
-        if entry_file:
-            Entry.entries_to_json_file(entry_file, entries)
-
-    return render_template('journal.j2', entries=sorted(entries, key=lambda e: e.timestamp))
+    return render_template('journal.j2', user=user, entries=sorted(entries, key=lambda e: e.timestamp))
 
 
 def main():
     global entry_file
-    global s3_token
+    global bucket_name
+    global user
 
     if 'ENTRY_FILE' in environ:
         entry_file = environ['ENTRY_FILE']
-    elif 'S3_TOKEN' in environ:
-        s3_token = environ['S3_TOKEN']
+    elif 'S3_BUCKET' in environ:
+        bucket_name = environ['S3_BUCKET']
     else:
-        raise ValueError("Need ENTRY_FILE or S3_TOKEN in environment")
+        raise ValueError("Need ENTRY_FILE or S3_BUCKET in environment")
 
     parser = ArgumentParser()
-    parser.add_argument('-d', '--debug', action='store_true', default=False)
+    parser.add_argument('--debug', '-d', action='store_true', default=False)
+    parser.add_argument('--user', '-u', action='store', default="Tim")
     args = parser.parse_args()
 
+    user = args.user
     app.run(debug=args.debug)
 
 
